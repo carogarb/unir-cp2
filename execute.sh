84a80f43ceadd93aa0044e9f271a70c7cb71c6ed
carogarb <cgarcia2315@gmail.com>
2025-07-17 10:52:23 +0200

    Adding sh file to login into Azure

---
+++ execute.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Stop execution on any error
+set -e
+
+function usage() {
+    echo "ERROR: Missing or invalid arguments!"
+    echo "Usage example: ./az-cli-login.sh SUBSCRIPTION_ID SERVICE_PRINCIPAL_ID SERVICE_PRINCIPAL_SECRET TENANT_ID"
+    exit 1
+}
+
+# Check if the right number of arguments were passed
+if [[ "$#" -ne 4 ]]; then
+    usage
+fi
+
+SUBSCRIPTION_ID=$1
+SERVICE_PRINCIPAL_ID=$2
+SERVICE_PRINCIPAL_SECRET=$3
+TENANT_ID=$4
+
+az login \
+    --service-principal \
+    --username "${SERVICE_PRINCIPAL_ID}" \
+    --password "${SERVICE_PRINCIPAL_SECRET}" \
+    --tenant "${TENANT_ID}"
+
+az account set -s "${SUBSCRIPTION_ID}"
+
+ ansible-playbook -i ansible/hosts.yml ansible/playbook.yml -vvv
